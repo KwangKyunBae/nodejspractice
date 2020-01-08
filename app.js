492fd767d990660695ad31d434bef5f4cc3aa8f1
KwangKyunBae <bae7359721@naver.com>
2020-01-08 14:38:23 +0900

    nodeJS+Express 웹 서버 설정

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static('public'))
+
 app.listen(3000,function(){
     console.log("start! express server on 3000 port")
 });
 
 console.log('end of server...')
+
+app.get('/',function(req,res) {
+    res.sendFile(__dirname + "/public/main.html")
+});
