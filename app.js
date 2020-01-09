e2ec67c74d330a6f0c14b9ba7528bba50fba1664
KwangKyunBae <bae7359721@naver.com>
2020-01-09 12:24:46 +0900

    Database 연동

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var express = require('express')
 var app = express()
 var bodyParser= require('body-parser')
+var mysql = require('mysql');
+var cors = require('cors')
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port : '3306',
+    password : 'dgu1234!',
+    database : 'jsman'
+})
+
+connection.connect()
+
+app.use(cors())
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
@@ -22,7 +35,22 @@ app.post('/email_post',function(req,res){
 });
 
 app.post('/ajax_send_email', function(req,res){
-    console.log(req.body.email);
-    var responseData = {'result' : 'ok','email' : req.body.email};
-    res.json(responseData);
+    var email = req.body.email;
+    var responseData = {};
+    
+    var query = connection.query('select name from user where email="' + email + '"',function(err, rows)
+    {
+        if(err) throw err;
+        if(rows[0]){
+            responseData.result = "ok";
+            responseData.name = rows[0].name;
+        }else {
+            responseData.result = "none";
+            responseData.name ="";
+        }
+
+        res.json(responseData);
+    })
+
+    
 });
